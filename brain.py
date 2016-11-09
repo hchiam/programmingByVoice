c38b7e023a3afdac257ba874ba72ceb120f90642
hchiam <hchiam@gmail.com>
2016-11-08 20:07:14 -0500

    adding functionality

--- brain.py
+++ brain.py
@@ -1,26 +1,67 @@
-def run(command, labelOutput):
+import re
+
+currentTabs = 0
+
+# parse is the main function here in the brain, and calls the other functions
+def parse(command, labelOutput):
     # initialize variables
     print command
-    output = ""
-    # consult command dictionary
-    if command[0] == "create":
-        name = command[2]
-        if command[1] == "file":
-            output = createFile(name, labelOutput)
-        elif command[1] == "variable":
-            output = createVariable(name)
-        elif command[1] == "function":
-            output = createFunction(name)
+    words = command.split(" ")
+    output = labelOutput
+    createCommandsList = ["variable", "function", "file", "import"]
+    editTabCommandsList = ["increase", "decrease", "left", "right", "more", "less", "add", "remove"]
+    # go through possible command keywords
+    checkIfCreatingSomething = re.match(r'.* create .*', command, re.I)
+    if checkIfCreatingSomething:
+        for keyword in createCommandsList:
+            matchFound = re.match( r'.* (create )?(a(n)? )?' + keyword +' (.*?) .*', command, re.I)
+            if matchFound:
+                name = matchFound.group(4)
+                if keyword == "variable":
+                    output = createVariable(name, labelOutput)
+                elif keyword == "function":
+                    output = createFunction(name, labelOutput)
+                elif keyword == "file":
+                    output = createFile(name, labelOutput)
+                elif keyword == "import":
+                    output = createImport(name, labelOutput)
+                break
+    else:
+        checkIfEditingTabs = re.match(r'.* tab(s)? .*', command, re.I)
+        checkIfEditingIndents = re.match(r'.* indent(s)? .*', command, re.I)
+        if checkIfEditingTabs or checkIfEditingIndents:
+            for keyword in editTabCommandsList:
+                matchFound = re.match( r'.* ' + keyword + '(a(n)? )?tab(s)? .*', command, re.I)
+                if matchFound:
+                    name = matchFound.group(1)
+                    if keyword == "increase" or keyword == "right" or keyword == "more" or keyword == "add":
+                        output = addTab(labelOutput)
+                    elif keyword == "decrease" or keyword == "left" or keyword == "less" or keyword == "remove":
+                        output = removeTab(labelOutput)
+                    break
+                matchFound = re.match( r'.* (a(n)? )?tab(s)? ' + keyword + '.*', command, re.I)
     return output
 
 def createFile(name, labelOutput):
     import io
     with io.FileIO(name + ".py", "w") as file:
         file.write(labelOutput)
-    return ""
+    return labelOutput
 
-def createVariable(name):
-    return name + "\n"
+def createVariable(name, labelOutput):
+    tabs = ("\t" * currentTabs)
+    return labelOutput + tabs + name + "\n"
 
-def createFunction(name):
-    return "def " + name + "(" + "):\n\t"
+def createFunction(name, labelOutput):
+    global currentTabs
+    currentTabs += 1
+    return labelOutput + "def " + name + "(" + "):\n\t"
+
+def createImport(name, labelOutput):
+    return "import " + name + "\n" + labelOutput
+
+def addTab(labelOutput):
+    return labelOutput + "\t"
+
+def removeTab(labelOutput):
+    return labelOutput.rstrip('\t')
--- gui.py
+++ gui.py
@@ -1,25 +1,30 @@
-from Tkinter import *
-import brain
+from Tkinter import * # to be able to create a GUI
+import brain # the connection to brain.py
 
-def parseCommand(words):
-    command = words.split(" ")
-    command = filter(None, command) # remove extraneous spaces
-    firstWord = command[0]
-    lastWord = command[-1]
+def parseCommand(command):
+    # check that sentence entered is "computer [...] please"
+    firstWord = command.split(" ")[0]
+    lastWord = command.split(" ")[-1]
     if firstWord == "computer" and lastWord == "please" and len(command) > 3:
-        output = brain.run(command[1:-1], labelOutput["text"])
+        # get "brain" to run command, update output text, and clear the sentence that was entered
+        output = brain.parse(command, labelOutput["text"])
+        outputStr.set(output)
         inputStr.set("")
-        outputStr.set(outputStr.get() + output)
 
+# create window
 window = Tk()
+window.wm_title("Voice Command Programmer")
 
-outputStr = StringVar()
-labelOutput = Label(window, textvariable=outputStr, justify=LEFT)
-labelOutput.pack(side=RIGHT)
-
+# add input Entry to window
 inputStr = StringVar()
 inputStr.trace("w", lambda name, index, mode, inputStr=inputStr: parseCommand(entryInput.get()))
 entryInput = Entry(window, textvariable=inputStr)
 entryInput.pack(side=LEFT)
 
+# add output Label to window
+outputStr = StringVar()
+labelOutput = Label(window, textvariable=outputStr, justify=LEFT)
+labelOutput.pack(side=RIGHT)
+
+# run window
 window.mainloop()

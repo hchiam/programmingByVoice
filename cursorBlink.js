2de4ccbcb56ddb540b7ea8523f160b10278587b2
hchiam <hchiam@gmail.com>
2016-12-30 23:37:43 -0500

    add note to fix

--- cursorBlink.js
+++ cursorBlink.js
@@ -10,6 +10,7 @@ function timerAction() {
             outputStr.innerText = outputStr.innerText.replace(/▂/g,""); // remove all instances of "▂"
             cursorVisible = false;
         } else {
+            // TODO: the following line is causing problems with spacing of lines:
             outputStr.innerText = outputStr.innerText.substring(0,indexOfCursor) + "▂" + outputStr.innerText.substring(indexOfCursor);
             cursorVisible = true;
         }

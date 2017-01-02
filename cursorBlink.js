f75883e6c7718a7c0bf3c90c1ec6466970337518
hchiam <hchiam@gmail.com>
2017-01-02 13:01:53 -0500

    fix cursor location for add at line

--- cursorBlink.js
+++ cursorBlink.js
@@ -10,7 +10,10 @@ function timerAction() {
             outputStr.innerText = outputStr.innerText.replace(/▂/g,""); // remove all instances of "▂"
             cursorVisible = false;
         } else {
-            var indexSplice = getIndexOfNthSubstring(outputStr.innerText, "\n", cursorLineNum-1);
+            var indexSplice = getIndexOfNthSubstring(outputStr.innerText, "\n", cursorLineNum-1) + 1;
+            if (indexSplice === 1) {
+                indexSplice = 0; // special case for first line (to make substring() not splice but just add instead)
+            }
             outputStr.innerText = outputStr.innerText.substring(0,indexSplice) + "▂" + outputStr.innerText.substring(indexSplice);
             cursorVisible = true;
         }
